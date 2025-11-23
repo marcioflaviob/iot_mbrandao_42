53833821c5e6b3c368aae82b44c07ed1affeea74
mbrandao <mbrandao@mbrandao.marcioflavio.com>
2025-11-23 19:36:05 +0100

    update

--- p3/scripts/install_argocd.sh
+++ p3/scripts/install_argocd.sh
@@ -12,8 +12,8 @@ echo "Initial admin password:"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 echo ""
 
-echo "Setting up port-forward for ArgoCD UI on http://localhost:8080"
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+echo "Setting up port-forward for ArgoCD UI on http://localhost:8081"
+kubectl port-forward svc/argocd-server -n argocd 8081:443 &
 
-echo "ArgoCD UI will be available at https://localhost:8080"
+echo "ArgoCD UI will be available at https://localhost:8081"
 echo "Username: admin"
